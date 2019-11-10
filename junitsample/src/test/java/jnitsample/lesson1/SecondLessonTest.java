91cf065724ca849c5e04301d48bf310eec1776c1
tuannn10 <57534464+tuannn10@users.noreply.github.com>
2019-11-10 14:51:04 +0700

    1

--- junitsample/src/test/java/jnitsample/lesson1/SecondLessonTest.java
+++ junitsample/src/test/java/jnitsample/lesson1/SecondLessonTest.java
@@ -7,11 +7,12 @@ import org.junit.Test;
 
 public class SecondLessonTest {
     @Test
-    int[] array={1,2,4,5,2,32,5,2,52,9};
-    int[] arrayC={2,4,2,32,2,52};
-    int[] arrayL={1,5,5,9};
-    SecondLesson a = new SecondLesson(array);
-    Assert.assertArrayEquals(arrayC,a.mangchan(array));
-    Assert.assertArrayEquals(arrayL,a.mangle(array));
-
+    public void checkArray() {
+        int[] array = new int[]{1, 2, 4, 5, 2, 32, 5, 2, 52, 9};
+        int[] arrayC = new int[]{2, 4, 2, 32, 2, 52};
+        int[] arrayL = new int[]{1, 5, 5, 9};
+        SecondLesson a = new SecondLesson(array);
+        Assert.assertArrayEquals(arrayC, a.mangchan(array));
+        Assert.assertArrayEquals(arrayL, a.mangle(array));
+    }
 }
--- junitsample/src/test/java/jnitsample/lesson1/ThirdLessonTest.java
+++ junitsample/src/test/java/jnitsample/lesson1/ThirdLessonTest.java
@@ -6,9 +6,10 @@ import org.junit.Test;
 
 public class ThirdLessonTest {
 @Test
-public int[] array={1,2,4,5,2,32,5,2,52,9};
-    int[] arraySnt={2,5};
-    ThirdLesson rd=new ThirdLesson(array,5);
-    Assert.assertArrayEquals(arraySnt,rd.SntLessX(array,5));
-
+public void checkArraySnt() {
+    int[] array = {1, 2, 4, 5, 2, 32, 5, 2, 52, 9};
+    int[] arraySnt = {2, 5};
+    ThirdLesson rd = new ThirdLesson(array, 5);
+    Assert.assertArrayEquals(arraySnt, rd.SntLessX(array, 5));
+}
 }

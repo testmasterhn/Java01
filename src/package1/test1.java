9f50f02f923d8cd8039d8b2f1ecde8615fc13547
tuannn10 <57534464+tuannn10@users.noreply.github.com>
2019-11-10 08:53:01 +0700

    1

--- src/package1/test1.java
+++ src/package1/test1.java
@@ -1,7 +1,11 @@
 package package1;
-
+import java.util.Scanner;
 public class test1 extends fistprograme {
     public static void main(String[] args){
-        fistprograme.count(5);
+        Scanner ss=new Scanner(System.in);
+        System.out.print("nhap so X can tim ");
+        int x =ss.nextInt();
+        int[] array= inputArray();
+        fistprograme.count(array,5);
     }
 }
